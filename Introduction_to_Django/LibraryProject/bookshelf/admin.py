3f86f0f20904f2b33cf99aafdeb8d1645cc7f159
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-01-09 18:40:55 +0000

    git commit -m "Add Book model and register it in admin"

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    # Columns to show in the admin list view
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Filters on the right sidebar
+    list_filter = ('author', 'publication_year')
+    
+    # Search bar fields
+    search_fields = ('title', 'author')
+    
+    # Optional: make publication year editable directly in the list view
+    list_editable = ('publication_year',)
+    
+    # Optional: sort by publication year by default
+    ordering = ('publication_year',)
+
+# Register the Book model with custom admin settings
+admin.site.register(Book, BookAdmin)
