fdf954a12e192bd7b31d758115da05d231046570
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-01-13 12:59:20 +0000

    git commit  -m "Implement user registration and authentication using Django's built-in forms"

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,6 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+
+# Signal to automatically create/update UserProfile
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        # Default role can be set here if needed
+        UserProfile.objects.create(user=instance, role='Member')
+    instance.userprofile.save()
+
+    
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
