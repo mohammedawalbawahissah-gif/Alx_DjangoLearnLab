e68f8ecc3f03ac15d5a7acdef555d6aecfdb7bc0
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-01-13 13:07:09 +0000

    git commit message

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,3 +16,8 @@ urlpatterns = [
         name='logout'
     ),
 ]
+urlpatterns = [
+    path('admin-dashboard/', views.admin_view, name='admin_view'),
+    path('librarian-dashboard/', views.librarian_view, name='librarian_view'),
+    path('member-dashboard/', views.member_view, name='member_view'),
+]
