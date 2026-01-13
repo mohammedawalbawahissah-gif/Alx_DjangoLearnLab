9682623c91e90bdc8560d9c7afbd8bb273330c61
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-01-13 11:00:59 +0000

    git commit -m "Update URL configurations and templates for relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 from . import views
-from .views import LibraryDetailView
+from .views import LibraryDetailView, list_books
+
+
 
 urlpatterns = [
-    # Function-based view URL
-    path('books/', views.list_books, name='list_books'),
-]
-    
-urlpatterns = [    
-    # Class-based view URL
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),              # Function-based view
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
 ]
