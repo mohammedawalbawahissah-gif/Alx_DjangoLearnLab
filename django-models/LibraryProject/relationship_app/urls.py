f0cc01f6201c2e0a8986490b348875e20e3482d9
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-01-13 10:42:09 +0000

    git commit -m "Update settings.py to set DEBUG and ALLOWED_HOSTS; Modify urls.py to include app URLs; Simplify library_detail.html and list_books.html templates; Refactor Book model in models.py"

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Function-based view URL
     path('books/', views.list_books, name='list_books'),
 
+
     # Class-based view URL
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,10 +5,11 @@ from .models import Book, Library
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 
 # Class-based view to display details of a single library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
