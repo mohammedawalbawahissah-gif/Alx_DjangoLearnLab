4575379b74510cdc42619c5e9434ea15b38bba52
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-01-13 08:33:05 +0000

    git commit message

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,10 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
+def query_books_by_author(author_name):
+    """Query all books by a specific author"""
+    author = Author.objects.get(name=author_name)
+    return author.books.all()
+
 # Query all books by a specific author
 author = Author.objects.first()
 books_by_author = Book.objects.filter(author=author)
@@ -12,6 +17,11 @@ def list_books_in_library(library_name):
     return library.books.all()
 
 from relationship_app.models import Library, Librarian
+
+def get_librarian_for_library(library_name):
+    """Retrieve the librarian for a library"""
+    library = Library.objects.get(name=library_name)
+    return library.librarian
 
 # Retrieve the librarian for a library
 library = Library.objects.first()
