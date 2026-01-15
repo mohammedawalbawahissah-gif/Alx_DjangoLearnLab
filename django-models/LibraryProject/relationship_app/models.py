c82082d4161fc571fea1dda3ff39c4083d00474e
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-01-15 08:45:28 +0000

    git commit -m "Add Author model to relationship_app/models.py"

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -32,6 +32,18 @@ class Library(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.ForeignKey(
+        'Library',
+        on_delete=models.CASCADE,
+        related_name='librarians'
+    )
+
+    def __str__(self):
+        return self.name
+
 
 
 class UserProfile(models.Model):
