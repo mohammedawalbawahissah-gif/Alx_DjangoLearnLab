5c98bef1fd55c8b335672ff3f560e9a859baacd1
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-01-19 20:29:38 +0000

    git commit -m "Implemented custom user model and updated related models to use it"

--- advanced_features_and_security/advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/advanced_features_and_security/bookshelf/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -8,3 +9,10 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class CustomUser(AbstractUser):
+    # Example custom field (optional but recommended)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
