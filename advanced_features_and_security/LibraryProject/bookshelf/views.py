a381f4aac12da4dfd778edc5186cef9460f24b53
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-01-20 08:46:25 +0000

    git commit -m "Implemented permission-based access control for book listing view"

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book  # make sure this matches your Book model
 
-# Create your views here.
+# View to list books
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
