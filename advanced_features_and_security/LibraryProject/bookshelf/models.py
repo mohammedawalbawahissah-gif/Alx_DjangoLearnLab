f46366adf98e8634f38cb5c1e9193213a0cf38ad
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-01-20 07:54:32 +0000

    git commit -m "Refactor models.py to improve code readability and maintainability"

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.conf import settings
-from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import AbstractUser, Group, Permission
 
 
@@ -12,18 +10,21 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+
 class CustomUser(AbstractUser):
     groups = models.ManyToManyField(
         Group,
-        related_name='bookshelf_user_set',  # custom related_name to avoid clash
+        related_name='bookshelf_user_set',
         blank=True,
         help_text='The groups this user belongs to.',
-        related_query_name='bookshelf_user'
+        related_query_name='bookshelf_user',
     )
+
     user_permissions = models.ManyToManyField(
         Permission,
         related_name='bookshelf_user_permissions_set',
         blank=True,
         help_text='Specific permissions for this user.',
-        related_query_name='bookshelf_user_permissions'
+        related_query_name='bookshelf_user_permissions',
     )
+
