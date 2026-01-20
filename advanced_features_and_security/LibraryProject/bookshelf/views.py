f60b9f6ff4e627f01a5e110246da7640c40e57b4
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-01-20 09:24:13 +0000

    git commit -m "Implemented permission-based access control for book listing view"

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book  
 from .forms import BookSearchForm
+from .forms import ExampleForm
+
 
 @permission_required('bookshelf.can_view', raise_exception=True)    
 def book_list(request):
