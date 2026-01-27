1ccdd75928e2c94638b2fc025851b55c8350d1f5
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-01-27 16:16:56 +0000

    git commit -m "Add Book model, serializer, view, and URL routing for API"

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+from .views import BookList
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('books/', BookList.as_view(), name='book-list'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics
 
 class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
