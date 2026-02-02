94012d71b99b918f6f65f868304c0b6704e0df79
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-02-02 23:23:11 +0000

    git commit message

--- advanced-api-project/library/views.py
+++ advanced-api-project/library/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend  # correct import
 from .models import Book
 from .serializers import BookSerializer
 from .filters import BookFilter
+from django_filters import rest_framework
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
