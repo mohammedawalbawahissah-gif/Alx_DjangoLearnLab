1b4c599cf36d15167e5c3d9d8d7d69a2aa3598e7
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-02-09 23:32:42 +0000

    git commit -m "Add user registration, login, and profile features

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -14,3 +13,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
