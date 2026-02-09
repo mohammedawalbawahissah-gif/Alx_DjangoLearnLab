1cf2f7e3ab72c79b0d2949acf181aa41c7de19c3
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-02-09 23:24:14 +0000

    git commit -m "Add user authentication views and templates"

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm
 
-# Create your views here.
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+
+    context = {
+    'u_form': u_form
+}
+    return render(request, 'blog/profile.html', context)
