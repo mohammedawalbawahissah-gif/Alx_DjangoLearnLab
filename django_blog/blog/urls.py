2844ace965f5a5cf9959911cc08d2f2b67b6d3d3
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-02-09 23:37:51 +0000

    git commit -m "Add user registration and authentication views and templates"

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,7 +5,10 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path(
+    'login/',
+    auth_views.LoginView.as_view(template_name='blog/login.html'),
+    name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
