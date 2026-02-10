2753e478b14128d75648716b33f7978982d03e84
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-02-10 00:40:52 +0000

    git commit -m "Add PostForm and Comment deletion view"

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from .models import Post  # Make sure Post model is imported
 from taggit.managers import TaggableManager
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -25,17 +26,6 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_posted = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
@@ -50,13 +40,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
-
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    # Add tags
-    tags = TaggableManager()
-
-    def __str__(self):
-        return self.title
-
