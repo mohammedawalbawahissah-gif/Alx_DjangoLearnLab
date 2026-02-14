0ffa820d33b41021bc5a3e5ade13a22506832ad8
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-02-14 00:11:16 +0000

    git commit -m "changes to post model"

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,16 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils import timezone
-from .models import Post  # Make sure Post model is imported
 from taggit.managers import TaggableManager
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_posted = models.DateTimeField(auto_now_add=True)
-    tags = TaggableManager()
 
     def __str__(self):
         return self.title
