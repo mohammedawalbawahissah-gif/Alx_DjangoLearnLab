af586c8c87589c27ddd74268743588f2c5e3a827
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-02-18 23:40:52 +0000

    git commit -m "Refactor UserRegisterSerializer for consistent formatting and improved readability"

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,18 +5,18 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    # <-- This is the required CharField
+    # This is the required CharField
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ("id", "username", "email", "password")
+        fields = ('id', 'username', 'email', 'password')
 
     def create(self, validated_data):
         user = get_user_model().objects.create_user(
-            username=validated_data["username"],
-            email=validated_data.get("email"),
-            password=validated_data["password"]
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
         )
         Token.objects.create(user=user)
         return user
