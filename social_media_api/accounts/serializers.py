6b12ffd775917006f9fc621b3c92d3d86eba3448
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-02-19 00:20:35 +0000

    git commit -m "Refactor RegisterSerializer to improve email field handling and remove redundant imports"

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,29 +4,35 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth import authenticate
+
 User = get_user_model()
 
 
 # Serializer for registering a new user
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
-    )
 
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password')
 
     def create(self, validated_data):
+        # Creates the user properly with hashed password
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data.get('email'),
             password=validated_data['password']
         )
-        Token.objects.create(user=user)  # create auth token automatically
+        # Create token for the user
+        Token.objects.create(user=user)
         return user
+
 
 
 # Serializer for returning user info
